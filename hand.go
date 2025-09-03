9c79b8b9cd580af46395fd71adf14e5febaf02f7
blakehulett7 <blake.hulett7@gmail.com>
2025-09-03 14:26:08 -0500

    taking out unused function

--- hand.go
+++ hand.go
@@ -77,32 +77,6 @@ func CreateNestedTargets(fusion Fusion, hand []int) []int {
 	return append(targets, fusion.ResultId)
 }
 
-func (app Bridge) NestedFusions(fusion Fusion, hand []int) []Fusion {
-	m1_found := false
-	m2_found := false
-	targets := []int{}
-
-	for _, card := range hand {
-		if !m1_found && fusion.Material1Id == card {
-			m1_found = true
-			continue
-		}
-
-		if !m2_found && fusion.Material2Id == card {
-			m2_found = true
-			continue
-		}
-
-		targets = append(targets, card)
-	}
-
-	nested_hand := append(targets, fusion.ResultId)
-	ColorPrint(Yellow, fmt.Sprintf("----Starting Nested Run %v----\n", nested_hand))
-	//app.BestFusion(nested_hand)
-
-	return []Fusion{}
-}
-
 func (app Bridge) PossibleFusions(subject int, targets []int, hand []int) []Fusion {
 	fmt.Println(subject, targets)
 
@@ -120,17 +94,4 @@ func (app Bridge) PossibleFusions(subject int, targets []int, hand []int) []Fusi
 	fmt.Println()
 
 	return fusions
-}
-
-func Sort(hand Hand) [5]int {
-	cards := hand[:]
-
-	slices.Sort(cards)
-
-	sorted := [5]int{}
-	for i := range 5 {
-		sorted[i] = cards[i]
-	}
-
-	return sorted
 }
